1cf25fad234916f812d9232da8b63127b9ca8983
Danilo Santos <danilosantos@dcc.ufba.br>
2014-01-17 18:19:16 -0300

    iniciando repositorio

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Aeroporto{
+struct ListaAviao *aviao;
+struct Aeroporto *prox;
+int garagem;
+}Aeroporto;
+
+typedef struct ListaAviao{
+char familia;
+int passageiros;
+int indice;
+} ListaAviao;
